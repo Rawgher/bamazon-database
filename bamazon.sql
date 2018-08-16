00c3eb17fbcb676155851a3b82bab112c9cef038
Roger <rdpender3@gmail.com>
2018-08-15 22:36:41 -0700

    set up bamazon table

--- bamazon.sql
+++ bamazon.sql
@@ -1,5 +1,17 @@
 CREATE DATABASE bamazon_db;
 
--- Use programming db for the following statements --
-
 use bamazon_db;
+
+CREATE TABLE products (
+	 item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
+  -- Make a string column called "artist" --
+	 product_name VARCHAR(20) NOT NULL,
+  -- Make an integer column called "score" --
+	 department_name VARCHAR(20) NOT NULL,
+     
+     price INTEGER(10) NOT NULL,
+     
+     stock_quantity INTEGER(10) NOT NULL
+);
+
+-- need to add 10 mock products here -- 
