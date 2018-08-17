660fb13d9c1195c3896ae063cba690f705f5cf2d
Roger <rdpender3@gmail.com>
2018-08-16 19:01:44 -0700

    changing more table parts

--- bamazon.sql
+++ bamazon.sql
@@ -3,7 +3,7 @@ CREATE DATABASE bamazon_db;
 use bamazon_db;
 
 CREATE TABLE products (
-	 item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
+	 item_id INT AUTO_INCREMENT NOT NULL,
 
 	 product_name VARCHAR(20) NOT NULL,
 
