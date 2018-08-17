8d06aec376da919b52b1e6bd8906b6081f71c70c
Roger <rdpender3@gmail.com>
2018-08-16 18:31:09 -0700

    added template for products

--- bamazon.sql
+++ bamazon.sql
@@ -4,9 +4,9 @@ use bamazon_db;
 
 CREATE TABLE products (
 	 item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
-  -- Make a string column called "artist" --
+
 	 product_name VARCHAR(20) NOT NULL,
-  -- Make an integer column called "score" --
+
 	 department_name VARCHAR(20) NOT NULL,
      
      price INTEGER(10) NOT NULL,
@@ -14,4 +14,6 @@ CREATE TABLE products (
      stock_quantity INTEGER(10) NOT NULL
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("", "", "", ,);
 -- need to add 10 mock products here -- 
