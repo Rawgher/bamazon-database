d6a3a0421bd00d9a9691bb003ce7339f382b822a
Roger <rdpender3@gmail.com>
2018-08-19 12:47:21 -0700

    added seed file and greatbay stuff to documents

--- bamazon.sql
+++ bamazon.sql
@@ -5,43 +5,14 @@ use bamazon_db;
 CREATE TABLE products (
 	 item_id INT AUTO_INCREMENT NOT NULL,
 
-	 product_name VARCHAR(20) NOT NULL,
+	 product_name VARCHAR(50) NOT NULL,
 
-	 department_name VARCHAR(20) NOT NULL,
+	 department_name VARCHAR(100) NOT NULL,
      
      price DECIMAL(10,2) NOT NULL,
      
      stock_quantity INTEGER(10) NOT NULL,
      
-     PRIMARY KEY (id)
+     PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", "", ,);
---
+++ bamazonseed.sql
@@ -0,0 +1,11 @@
+INSERT INTO bamazon_db.products (product_name, department_name, price, stock_quantity)
+VALUES ("Xbox One", "Electronics", 399.99, 15),
+("Playstation 4", "Electronics", 299.99, 10),
+("Litter Robot III", "Pets", 499.99, 7),
+("Ergonomic Desk", "Household Appliances", 299.96, 15),
+("Game of Thrones", "Books", 15.99, 200),
+("Futuristic Desk Lamp", "Household Appliances", 149.55, 17),
+("Mechanical Keyboard", "Electronics", 99.99, 8),
+("Coding For Dummies", "Books", 19.99, 200),
+("Cracking the Coding Interview", "Books", 18.95, 20),
+("Bose Desk Speakers", "Electronics", 199.99, 20);
