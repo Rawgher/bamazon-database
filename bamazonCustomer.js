9d2c8ac156b078ec4754d6af2282d974112e3999
Roger <rdpender3@gmail.com>
2018-08-20 21:45:27 -0700

    done with customer version

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -68,189 +68,44 @@ function start() {
       ])
       .then(function(answer) {
         // get the information of the chosen item
+        if (answer.buy > 0 ) {
+        let chosenItem;
         var components = answer.choice.trim().split(':');
         for (var k = 0; k < results.length; k++) {
           // test if results id = the first element of the components array (ie: the id)
-          if (results[k].id === parseInt(components[0])) {
+          if (results[k].item_id === parseInt(components[0])) {
             chosenItem = results[k];
           }
         }
+
+        if (parseInt(chosenItem.stock_quantity) < parseInt(answer.buy)) {
+          console.log("\nWe do not have enough products in stock to match your order.\nPlease check back after we restock!");
+          connection.end();
+        } else {
+          let newStock = chosenItem.stock_quantity - answer.buy;
+          let updateStock = [
+            {
+              stock_quantity: newStock
+            },
+            {
+              item_id: components[0]
+            }
+          ];
+          connection.query(
+            "UPDATE products SET ? WHERE ?", updateStock, function(error) {
+              if (error) throw error;
+              console.log("\nWe've received your order");
+            });
+
+        let totalCost = parseInt(answer.buy) * chosenItem.price;
+        console.log("\nOur system shows that you have ordered " + answer.buy + " " + chosenItem.product_name +".\nYour cart total was $" + totalCost);
+        connection.end()
+        }
+
+      } else {
+        console.log("\nYou need to choose a proper amount to buy. Try again")
+        connection.end()
+      }
       })
     })
   }
-
-  //       // determine if bid was high enough
-  //       if (chosenItem.highest_bid < parseInt(answer.bid)) {
-  //         // bid was high enough, so update db, let the user know, and start over
-  //         connection.query(
-  //           "UPDATE auctions SET ? WHERE ?",
-  //           [
-  //             {
-  //               highest_bid: answer.bid
-  //             },
-  //             {
-  //               id: chosenItem.id
-  //             }
-  //           ],
-  //           function(error) {
-  //             if (error) throw err;
-  //             console.log("Bid placed successfully!");
-  //             start();
-  //           }
-  //         );
-  //       }
-  //       else {
-  //         // bid wasn't high enough, so apologize and start over
-  //         console.log("Your bid was too low. Try again...");
-  //         start();
-  //       }
-  //     });
-  // });
-  
-  
-  // inquirer
-  //   .prompt({
-  //     name: "postOrBid",
-  //     type: "rawlist",
-  //     message: "Here is what we have for sale. Please choose an item or you can exit the store.",
-  //     choices: ["POST", "BID"]
-  //   })
-  //   .then(function(answer) {
-  //     // based on their answer, either call the bid or the post functions
-  //     if (answer.postOrBid.toUpperCase() === "POST") {
-  //       postAuction();
-  //     }
-  //     else {
-  //       bidAuction();
-  //     }
-  //   });
-// }
-
-// function to handle posting new items up for auction
-// function postAuction() {
-//   // prompt for info about the item being put up for auction
-//   inquirer
-//     .prompt([
-//       {
-//         name: "item",
-//         type: "input",
-//         message: "What is the item you would like to submit?"
-//       },
-//       {
-//         name: "category",
-//         type: "input",
-//         message: "What category would you like to place your auction in?"
-//       },
-//       {
-//         name: "startingBid",
-//         type: "input",
-//         message: "What would you like your starting bid to be?",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       // when finished prompting, insert a new item into the db with that info
-//       connection.query(
-//         "INSERT INTO auctions SET ?",
-//         {
-//           item_name: answer.item,
-//           category: answer.category,
-//           starting_bid: answer.startingBid,
-//           highest_bid: answer.startingBid
-//         },
-//         function(err) {
-//           if (err) throw err;
-//           console.log("Your auction was created successfully!");
-//           // re-prompt the user for if they want to bid or post
-//           start();
-//         }
-//       );
-//     });
-// }
-
-// function bidAuction() {
-//   // query the database for all items being auctioned
-//   connection.query("SELECT * FROM auctions", function(err, results) {
-//     if (err) throw err;
-//     // once you have the items, prompt the user for which they'd like to bid on
-//     inquirer
-//       .prompt([
-//         {
-//           name: "choice",
-//           type: "rawlist",
-//           choices: function() {
-//             var choiceArray = [];
-//             for (var i = 0; i < results.length; i++) {
-//               choiceArray.push(results[i].item_name);
-//             }
-//             return choiceArray;
-//           },
-//           message: "What auction would you like to place a bid in?"
-//         },
-//         {
-//           name: "bid",
-//           type: "input",
-//           message: "How much would you like to bid?"
-//         }
-//       ])
-//       .then(function(answer) {
-//         // get the information of the chosen item
-//         var chosenItem;
-//         for (var i = 0; i < results.length; i++) {
-//           if (results[i].item_name === answer.choice) {
-//             chosenItem = results[i];
-//           }
-//         }
-
-//         // determine if bid was high enough
-//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//           // bid was high enough, so update db, let the user know, and start over
-//           connection.query(
-//             "UPDATE auctions SET ? WHERE ?",
-//             [
-//               {
-//                 highest_bid: answer.bid
-//               },
-//               {
-//                 id: chosenItem.id
-//               }
-//             ],
-//             function(error) {
-//               if (error) throw err;
-//               console.log("Bid placed successfully!");
-//               start();
-//             }
-//           );
-//         }
-//         else {
-//           // bid wasn't high enough, so apologize and start over
-//           console.log("Your bid was too low. Try again...");
-//           start();
-//         }
-//       });
-//   });
-// }
-// The app should then prompt users with two messages.
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-
-
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
