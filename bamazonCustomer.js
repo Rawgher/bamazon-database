46daa6f6ec01d452375a06202f8c1707e0db49d1
Roger <rdpender3@gmail.com>
2018-08-20 23:15:05 -0700

    added comments and formatted both docs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,59 +1,50 @@
 // Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
+//setting up required npms
 let inquirer = require("inquirer");
 
 let mysql = require("mysql");
 
 const cTable = require("console.table");
 
+//setting up mysql connection
 let connection = mysql.createConnection({
+
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "root",
   database: "bamazon_db"
+
 });
 
-
+//initiall connection to database
 connection.connect(function (err) {
+
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   start();
+
 });
 
-// function which prompts the user for what action they should take
+// function which prompts the user for what action they would like to take
 function start() {
 
   connection.query("SELECT * FROM products", function (err, results) {
     if (err) throw err;
 
-    var choiceArray = [];
-    for (var i = 0; i < results.length; i++) {
-      // push into the array a string like this: "2: Car"
-      // choiceArray.push("Item Id: " + results[i].item_id + ", Item Name: " + results[i].product_name + ", $" + results[i].price);
-      choiceArray.push(results[i].item_id + results[i].product_name + results[i].price);
-    }
-
-
     console.log("\nHere is everything we have for sale at Bamazon.\n")
     console.table(results);
 
-
-
     var itemArray = [];
     for (var j = 0; j < results.length; j++) {
-      // push into the array a string like this: "2: Car"
+
       itemArray.push(results[j].item_id + ": " + results[j].product_name);
+
     }
-    // once you have the items, prompt the user for which they'd like to bid on
+
     inquirer
       .prompt([
+
         {
           name: "choice",
           type: "list",
@@ -65,24 +56,44 @@ function start() {
           type: "input",
           message: "How many would you like to buy?"
         }
+
       ])
+      // after an item is selected and a quantity is chosen, function runs
       .then(function (answer) {
-        // get the information of the chosen item
+
+        // function that runs if the user inputs a number and it is more than 0
         if (answer.buy > 0) {
+
+          // get the information of the chosen item and store it
           let chosenItem;
           var components = answer.choice.trim().split(':');
+
           for (var k = 0; k < results.length; k++) {
+
             // test if results id = the first element of the components array (ie: the id)
             if (results[k].item_id === parseInt(components[0])) {
-              chosenItem = results[k];
-            }
-          }
 
+              chosenItem = results[k];
+
+            };
+
+          };
+
+          // only runs if there is a there are not enough items in stock
           if (parseInt(chosenItem.stock_quantity) < parseInt(answer.buy)) {
+
             console.log("\nWe do not have enough products in stock to match your order.\nPlease check back after we restock!");
             connection.end();
-          } else {
+
+          }
+
+          // else statement that runs if there are enough items in stock
+          else {
+
+            // subtracting the amount purchased from the current stock
             let newStock = chosenItem.stock_quantity - answer.buy;
+
+            // setting up variable that will change the database
             let updateStock = [
               {
                 stock_quantity: newStock
@@ -91,21 +102,34 @@ function start() {
                 item_id: components[0]
               }
             ];
-            connection.query(
-              "UPDATE products SET ? WHERE ?", updateStock, function (error) {
-                if (error) throw error;
-                console.log("\nWe've received your order.");
-              });
 
+            // updating the mySQL database based on user's input
+            connection.query("UPDATE products SET ? WHERE ?", updateStock, function (error) {
+
+              if (error) throw error;
+              console.log("\nWe've received your order.\n");
+
+            });
+
+            // calculating cost to user based off of how mnay products bought
             let totalCost = parseInt(answer.buy) * chosenItem.price;
-            console.log("\nOur system shows that you have ordered " + answer.buy + " " + chosenItem.product_name + ".\nYour cart total was $" + totalCost +".");
+            console.log("\nOur system shows that you have ordered " + answer.buy + " " + chosenItem.product_name + ".\nYour cart total was $" + totalCost + ".\n");
             connection.end()
-          }
 
-        } else {
+          };
+
+        }
+
+        // else statement that runs if user does not input a number or their choice is equal to 0
+        else {
+
           console.log("\nYou need to choose a proper amount to buy. Try again.")
           connection.end()
-        }
-      })
-  })
-}
+
+        };
+
+      });
+
+  });
+
+};
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,92 +1,115 @@
+// setting up required npms
 let inquirer = require("inquirer");
 
 let mysql = require("mysql");
 
 const cTable = require("console.table");
 
-var command = process.argv.slice(2).join(" ");
-
+// setting up mySQL connection
 let connection = mysql.createConnection({
+    
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "root",
     database: "bamazon_db"
+
 });
 
-
+// setting up initial connection
 connection.connect(function (err) {
+
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     start();
+
 });
 
+// initial display that prompts user to choose what they would like to do
 function start() {
+
     inquirer.prompt({
+
         name: "managerOptions",
         type: "list",
         message: "What would you like to do, manager?",
         choices: ["View All Products", "View Low Inventory", "Add to Inventory", "Add New Products", "Exit"]
+
     }).then(function (answers) {
+        
         if (answers.managerOptions.toLowerCase() === "view all products") {
+            
             forSale()
+
         }
         else if (answers.managerOptions.toLowerCase() === "view low inventory") {
+
             lowInv()
+
         }
         else if (answers.managerOptions.toLowerCase() === "add to inventory") {
+
             addInv()
+
         }
         else if (answers.managerOptions.toLowerCase() === "add new products") {
+
             addProduct()
+
         } else {
+
             connection.end();
-        }
-    })
-}
 
+        };
+
+    });
+
+};
+
+// function that displays everything that is on sale
 function forSale() {
-    connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
 
+    connection.query("SELECT * FROM products", function (err, results) {
+
+        if (err) throw err;
         console.log("\nHere is everything currently on sale at Bamazon.\n")
         console.table(results);
         start();
-    })
+
+    });
+
 };
 
+// function that shows everything on sale that has less than 5 items
 function lowInv() {
 
     connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, results) {
 
         if (err) throw err;
-
         console.log("\nHere are all items on Bamazon with that's stock is lower than 5.\n")
         console.table(results);
         start();
-    })
 
-}
+    });
 
+};
+
+// function that allows the manager to add to an items stock
 function addInv() {
 
     connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
 
+        if (err) throw err;
         var itemArray = [];
         for (var j = 0; j < results.length; j++) {
-            // push into the array a string like this: "2: Car"
-            itemArray.push(results[j].item_id + ": " + results[j].product_name);
-        }
 
-        inquirer
-            .prompt([
+            itemArray.push(results[j].item_id + ": " + results[j].product_name);
+            
+        };
+
+        // prompt asking which item and how much they would like to add to it
+        inquirer.prompt([
+
                 {
                     name: "choice",
                     type: "list",
@@ -98,17 +121,27 @@ function addInv() {
                     type: "input",
                     message: "How many would you like to add?"
                 }
+
             ]).then(function (answer) {
-                // when finished prompting, insert a new item into the db with that info
+
+                // will only run if the user inputs a number and it is higher than 0
                 if (answer.stock > 0) {
+
+                              // get the information of the chosen item and store it
                     let chosenItem;
                     var components = answer.choice.trim().split(':');
+
                     for (var k = 0; k < results.length; k++) {
+
                         // test if results id = the first element of the components array (ie: the id)
                         if (results[k].item_id === parseInt(components[0])) {
+
                             chosenItem = results[k];
-                        }
-                    }
+
+                        };
+                    };
+
+                    // setting up update of stock
                     let updateSock = [
                         {
                             stock_quantity: parseInt(answer.stock) + chosenItem.stock_quantity
@@ -117,63 +150,82 @@ function addInv() {
                             item_id: components[0]
                         }
                     ];
-                    connection.query(
-                        "UPDATE products SET ? WHERE ?", updateSock, function (error) {
+
+                    // query to update stock on the database
+                    connection.query("UPDATE products SET ? WHERE ?", updateSock, function (error) {
+
                             if (error) throw error;
                             console.log("\nThe product was successfully restocked!\n");
                             start();
-                        }
-                    );
-                } else {
+
+                        });
+
+                }
+
+                // else statement that runs if user does not input a proper amount
+                else {
+
                     console.log("\nYou need to input a proper amount to update. Try again\n")
                     connection.end();
-                }
-            })
+
+                };
+
+            });
+
     });
+
 };
 
-
-
+// function to add a completely new product to the database
 function addProduct() {
 
+    // inquirer prompt that takes all the input needed to add item to the database
     inquirer.prompt([
-            {
-                name: "itemName",
-                type: "input",
-                message: "Please enter the item you want to add!"
-            },
-            {
-                name: "departmentName",
-                type: "input",
-                message: "Please enter a department name!"
-            },
-            {
-                name: "price",
-                type: "input",
-                message: "How much does this item cost (to two decimal places)?"
-            },
-            {
-                name: "stock",
-                type: "input",
-                message: "How much of this product would you like to add?"
-            }
-        ]).then(function (answer) {
-            // when finished prompting, insert a new item into the db with that info
 
-            var sqlString = "INSERT INTO products SET ?";
-            var replacements = {
-                product_name: answer.itemName,
-                department_name: answer.departmentName,
-                price: parseFloat(answer.price),
-                stock_quantity: parseInt(answer.stock)
-            };
-            connection.query(sqlString, replacements, function (err) {
-                if (err) throw err;
-                console.log("\nThe product has been successfully added!\n");
-                // re-prompt the user for if they want to bid or post
-                start();
-            });
+        {
+            name: "itemName",
+            type: "input",
+            message: "Please enter the item you want to add!"
+        },
+        {
+            name: "departmentName",
+            type: "input",
+            message: "Please enter a department name!"
+        },
+        {
+            name: "price",
+            type: "input",
+            message: "How much does this item cost (to two decimal places)?"
+        },
+        {
+            name: "stock",
+            type: "input",
+            message: "How much of this product would you like to add?"
+        }
+
+    ]).then(function (answer) {
+        // when finished prompting, insert a new item into the db with the input info
+
+        var sqlString = "INSERT INTO products SET ?";
+
+        var replacements = {
+
+            product_name: answer.itemName,
+            department_name: answer.departmentName,
+            price: parseFloat(answer.price),
+            stock_quantity: parseInt(answer.stock)
+
+        };
+
+        // query that adds the new item
+        connection.query(sqlString, replacements, function (err) {
+
+            if (err) throw err;
+            console.log("\nThe product has been successfully added!\n");
+            start();
+
         });
-    };
 
-
+    });
+    
+};
