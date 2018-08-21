f8484d1d82935a162f88c3d289fc463f7d4a5640
Roger <rdpender3@gmail.com>
2018-08-20 22:52:12 -0700

    finished manager app

--- bamazonManager.js
+++ bamazonManager.js
@@ -33,7 +33,7 @@ function start() {
         type: "list",
         message: "What would you like to do, manager?",
         choices: ["View All Products", "View Low Inventory", "Add to Inventory", "Add New Products", "Exit"]
-    }).then(function(answers) {
+    }).then(function (answers) {
         if (answers.managerOptions.toLowerCase() === "view all products") {
             forSale()
         }
@@ -43,7 +43,7 @@ function start() {
         else if (answers.managerOptions.toLowerCase() === "add to inventory") {
             addInv()
         }
-        else if (answers.managerOptions.toLowerCase() === "add new product") {
+        else if (answers.managerOptions.toLowerCase() === "add new products") {
             addProduct()
         } else {
             connection.end();
@@ -64,7 +64,7 @@ function forSale() {
 function lowInv() {
 
     connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, results) {
-        
+
         if (err) throw err;
 
         console.log("\nHere are all items on Bamazon with that's stock is lower than 5.\n")
@@ -75,60 +75,60 @@ function lowInv() {
 }
 
 function addInv() {
-    
+
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
 
-    var itemArray = [];
-    for (var j = 0; j < results.length; j++) {
-      // push into the array a string like this: "2: Car"
-      itemArray.push(results[j].item_id + ": " + results[j].product_name);
-    }
+        var itemArray = [];
+        for (var j = 0; j < results.length; j++) {
+            // push into the array a string like this: "2: Car"
+            itemArray.push(results[j].item_id + ": " + results[j].product_name);
+        }
 
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "list",
-          choices: itemArray,
-          message: "Please select the item id you are interested in!"
-        },
-        {
-          name: "stock",
-          type: "input",
-          message: "How many would you like to add?"
-        } 
-      ]).then(function(answer) {
-        // when finished prompting, insert a new item into the db with that info
-        if (answer.stock > 0) {
-            let chosenItem;
-            var components = answer.choice.trim().split(':');
-            for (var k = 0; k < results.length; k++) {
-              // test if results id = the first element of the components array (ie: the id)
-              if (results[k].item_id === parseInt(components[0])) {
-                chosenItem = results[k];
-              }
-            }
-        let updateSock = [
-              {
-                stock_quantity: parseInt(answer.stock) + chosenItem.stock_quantity
-              },
-              {
-                item_id: components[0]
-              }
-            ];
-        connection.query(
-            "UPDATE products SET ? WHERE ?", updateSock, function(error) {
-            if (error) throw error;
-            console.log("\nThe product was successfully restocked!");
-            start();
-          }
-        );
-      } else {
-          console.log("\nYou need to input a proper amount to update. Try again")
-          connection.end();
-      }
-  })
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "list",
+                    choices: itemArray,
+                    message: "Please select the item id you are interested in!"
+                },
+                {
+                    name: "stock",
+                    type: "input",
+                    message: "How many would you like to add?"
+                }
+            ]).then(function (answer) {
+                // when finished prompting, insert a new item into the db with that info
+                if (answer.stock > 0) {
+                    let chosenItem;
+                    var components = answer.choice.trim().split(':');
+                    for (var k = 0; k < results.length; k++) {
+                        // test if results id = the first element of the components array (ie: the id)
+                        if (results[k].item_id === parseInt(components[0])) {
+                            chosenItem = results[k];
+                        }
+                    }
+                    let updateSock = [
+                        {
+                            stock_quantity: parseInt(answer.stock) + chosenItem.stock_quantity
+                        },
+                        {
+                            item_id: components[0]
+                        }
+                    ];
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?", updateSock, function (error) {
+                            if (error) throw error;
+                            console.log("\nThe product was successfully restocked!\n");
+                            start();
+                        }
+                    );
+                } else {
+                    console.log("\nYou need to input a proper amount to update. Try again\n")
+                    connection.end();
+                }
+            })
     });
 };
 
@@ -136,23 +136,44 @@ function addInv() {
 
 function addProduct() {
 
-    inquirer.prompt(questions).then(function(answer) {
-        // when finished prompting, insert a new item into the db with that info
-        
-        var sqlString = "INSERT INTO auctions SET ?";
-        var replacements = {
-          item_name: answer.item,
-          category: answer.category,
-          starting_bid: answer.startingBid,
-          highest_bid: answer.startingBid
-        };
-        connection.query(sqlString, replacements, function(err) {
-            if (err) throw err;
-            console.log("Your auction was created successfully!");
-            // re-prompt the user for if they want to bid or post
-            start();
-          }
-        );
-      });
-  }
+    inquirer.prompt([
+            {
+                name: "itemName",
+                type: "input",
+                message: "Please enter the item you want to add!"
+            },
+            {
+                name: "departmentName",
+                type: "input",
+                message: "Please enter a department name!"
+            },
+            {
+                name: "price",
+                type: "input",
+                message: "How much does this item cost (to two decimal places)?"
+            },
+            {
+                name: "stock",
+                type: "input",
+                message: "How much of this product would you like to add?"
+            }
+        ]).then(function (answer) {
+            // when finished prompting, insert a new item into the db with that info
+
+            var sqlString = "INSERT INTO products SET ?";
+            var replacements = {
+                product_name: answer.itemName,
+                department_name: answer.departmentName,
+                price: parseFloat(answer.price),
+                stock_quantity: parseInt(answer.stock)
+            };
+            connection.query(sqlString, replacements, function (err) {
+                if (err) throw err;
+                console.log("\nThe product has been successfully added!\n");
+                // re-prompt the user for if they want to bid or post
+                start();
+            });
+        });
+    };
+
 
