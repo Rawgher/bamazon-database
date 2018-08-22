6928a352a910a17afc5a0db794fafc74dfa7974e
Roger <rdpender3@gmail.com>
2018-08-21 23:48:06 -0700

    just need to do product sales section

---
+++ bamazonSupervisor.js
@@ -0,0 +1,101 @@
+// view product sales by department
+// should show total profit (calculated from over head costs and product sales)
+// need a custom alias?
+
+
+
+// setting up required npms
+let inquirer = require("inquirer");
+
+let mysql = require("mysql");
+
+const cTable = require("console.table");
+
+// setting up mySQL connection
+let connection = mysql.createConnection({
+    
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
+
+});
+
+// setting up initial connection
+connection.connect(function (err) {
+
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+
+});
+
+// initial display that prompts user to choose what they would like to do
+function start() {
+
+    inquirer.prompt({
+
+        name: "supervisorOptions",
+        type: "list",
+        message: "What would you like to do, supreme leader?",
+        choices: ["View Product Sales by Department", "Add a New Department", "Exit"]
+
+    }).then(function (answers) {
+        
+        if (answers.supervisorOptions.toLowerCase() === "view product sales by department") {
+            
+            productSales()
+
+        }
+        else if (answers.supervisorOptions.toLowerCase() === "add a new department") {
+
+            newDepartment()
+
+        } else {
+
+            connection.end();
+
+        };
+
+    });
+
+};
+
+function productSales () {
+
+}
+
+// function to add a new department
+function newDepartment () {
+    inquirer.prompt([
+
+        {
+            name: "departmentName",
+            type: "input",
+            message: "Please enter the department you want to add!"
+        }
+
+    ]).then(function (answer) {
+        // when finished prompting, insert a new department into the db that starts with 0 overhead coset
+
+        var sqlString = "INSERT INTO departments SET ?";
+
+        var replacements = {
+
+            department_name: answer.departmentName,
+            over_head_cost: 0
+
+        };
+
+        // query that adds the new item to the database
+        connection.query(sqlString, replacements, function (err) {
+
+            if (err) throw err;
+            console.log("\nThe department has been successfully added!\n");
+            start();
+
+        });
+
+    });
+}
