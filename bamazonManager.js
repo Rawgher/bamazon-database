26406720a92c43874e17589871b6d228c35919c3
Roger <rdpender3@gmail.com>
2018-08-23 18:34:56 -0700

    added some number validation

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,7 +7,7 @@ const cTable = require("console.table");
 
 // setting up mySQL connection
 let connection = mysql.createConnection({
-    
+
     host: "localhost",
     port: 3306,
     user: "root",
@@ -36,9 +36,9 @@ function start() {
         choices: ["View All Products", "View Low Inventory", "Add to Inventory", "Add New Products", "Exit"]
 
     }).then(function (answers) {
-        
+
         if (answers.managerOptions.toLowerCase() === "view all products") {
-            
+
             forSale()
 
         }
@@ -104,73 +104,73 @@ function addInv() {
         for (var j = 0; j < results.length; j++) {
 
             itemArray.push(results[j].item_id + ": " + results[j].product_name);
-            
+
         };
 
         // prompt asking which item and how much they would like to add to it
         inquirer.prompt([
 
-                {
-                    name: "choice",
-                    type: "list",
-                    choices: itemArray,
-                    message: "Please select the item id you are interested in!"
-                },
-                {
-                    name: "stock",
-                    type: "input",
-                    message: "How many would you like to add?"
-                }
+            {
+                name: "choice",
+                type: "list",
+                choices: itemArray,
+                message: "Please select the item id you are interested in!"
+            },
+            {
+                name: "stock",
+                type: "input",
+                message: "How many would you like to add?"
+            }
 
-            ]).then(function (answer) {
+        ]).then(function (answer) {
 
-                // will only run if the user inputs a number and it is higher than 0
-                if (answer.stock > 0) {
+            // will only run if the user inputs a number and it is higher than 0
+            if (answer.stock > 0) {
 
-                              // get the information of the chosen item and store it
-                    let chosenItem;
-                    var components = answer.choice.trim().split(':');
+                // get the information of the chosen item and store it
+                let chosenItem;
+                var components = answer.choice.trim().split(':');
 
-                    for (var k = 0; k < results.length; k++) {
+                for (var k = 0; k < results.length; k++) {
 
-                        // test if results id = the first element of the components array (ie: the id)
-                        if (results[k].item_id === parseInt(components[0])) {
+                    // test if results id = the first element of the components array (ie: the id)
+                    if (results[k].item_id === parseInt(components[0])) {
 
-                            chosenItem = results[k];
+                        chosenItem = results[k];
 
-                        };
                     };
-
-                    // setting up update of stock
-                    let updateSock = [
-                        {
-                            stock_quantity: parseInt(answer.stock) + chosenItem.stock_quantity
-                        },
-                        {
-                            item_id: components[0]
-                        }
-                    ];
-
-                    // query to update stock on the database
-                    connection.query("UPDATE products SET ? WHERE ?", updateSock, function (error) {
-
-                            if (error) throw error;
-                            console.log("\nThe product was successfully restocked!\n");
-                            start();
-
-                        });
-
-                }
-
-                // else statement that runs if user does not input a proper amount
-                else {
-
-                    console.log("\nYou need to input a proper amount to update. Try again\n")
-                    connection.end();
-
                 };
 
-            });
+                // setting up update of stock
+                let updateSock = [
+                    {
+                        stock_quantity: parseInt(answer.stock) + chosenItem.stock_quantity
+                    },
+                    {
+                        item_id: components[0]
+                    }
+                ];
+
+                // query to update stock on the database
+                connection.query("UPDATE products SET ? WHERE ?", updateSock, function (error) {
+
+                    if (error) throw error;
+                    console.log("\nThe product was successfully restocked!\n");
+                    start();
+
+                });
+
+            }
+
+            // else statement that runs if user does not input a proper amount
+            else {
+
+                console.log("\nYou need to input a proper amount to update. Try again\n")
+                connection.end();
+
+            };
+
+        });
 
     });
 
@@ -195,12 +195,24 @@ function addProduct() {
         {
             name: "price",
             type: "input",
-            message: "How much does this item cost?"
+            message: "How much does this item cost?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
         },
         {
             name: "stock",
             type: "input",
-            message: "How much of this product would you like to add?"
+            message: "How much of this product would you like to add?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
         }
 
     ]).then(function (answer) {
@@ -228,5 +240,5 @@ function addProduct() {
         });
 
     });
-    
+
 };
